e8f81dfbd34a34664056af691d300bb227ec71e5
syukrontanzilah <syukrontanzilah@gmail.com>
2020-09-19 13:45:21 +0700

    add login

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './App.css'
+import Login from './Login'
+
+
+const App = () => {
+  return(
+<div  className="app">
+<Login/>
+
+
+{/* login with spotify button */}
+
+
+
+
+          </div>
+  )
+}
+
+export default App
---
+++ src/Login.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Login.css'
+
+function Login() {
+    return (
+        <div className="login">
+            <img width="100px" src="https://berndvoss.com/wp-content/uploads/2018/10/spotify-transp-white-1.png"/>
+          <a href="#">Login With Spotify</a>
+        </div>
+    )
+}
+
+export default Login
+
